18ed0b7551e0f746fcf7b87c9b52ccc5db7e5c6d
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-09-28 17:00:57 +0200

    add left menu panel

---
+++ src/components/LeftMenu.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="column is-narrow side-menu is-hidden-touch">
+    <section class="section">
+      <div class="tile box is-radiusless">
+        <aside class="menu" id="navMenu"></aside>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class LeftMenu extends Vue {}
+</script>
