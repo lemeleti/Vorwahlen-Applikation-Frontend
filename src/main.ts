5cc01962df22068fd1a2bca3db5c7db277795f0e
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-09-28 16:58:19 +0200

    register font awesome icons

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,21 @@ import router from "./router";
 import store from "./store";
 import Buefy from "buefy";
 import "buefy/dist/buefy.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faHome, faUser, faEdit, faCogs, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
+library.add(faHome, faUser, faEdit, faCogs, faSignOutAlt);
+
+Vue.component("vue-fontawesome", FontAwesomeIcon);
 Vue.config.productionTip = false;
+Vue.use(Buefy, {
+  defaultIconComponent: "vue-fontawesome",
+  defaultIconPack: "fas",
+});
 
 new Vue({
   router,
   store,
   render: (h) => h(App),
 }).$mount("#app");
-
-Vue.use(Buefy);
