c2061a45477f3540450a4ebec485f58030436686
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-09-29 21:21:28 +0200

    add modal for additional subject info

---
+++ src/components/SubjectInfoModal.vue
@@ -0,0 +1,49 @@
+<template>
+  <b-modal v-model="syncedIsModalActive">
+    <div class="box is-radiusless">
+      <div class="block">
+        <p class="title">{{ title }}</p>
+      </div>
+      <p class="subtitle">Detailierte Übersicht</p>
+      <div class="content">
+        <div class="table-container">
+          <table class="table">
+            <tbody>
+              <tr>
+                <td id="fach"><b>Fach</b></td>
+              </tr>
+              <tr>
+                <td id="modulverantwortung"><b>Modulverantwortung</b></td>
+              </tr>
+              <tr>
+                <td id="kompetenzen"><b>Kompetenzen</b></td>
+              </tr>
+              <tr>
+                <td id="modulinhalte"><b>Modul- / Lerninhalte</b></td>
+              </tr>
+              <tr>
+                <td id="unterrichtssprache"><b>Unterrichtssprache</b></td>
+              </tr>
+              <tr>
+                <td id="modulstruktur"><b>Modulstruktur</b></td>
+              </tr>
+              <tr>
+                <td id="leistungsnachweise"><b>Leistungsnachweise</b></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </b-modal>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, PropSync } from "vue-property-decorator";
+
+@Component
+export default class SubjectInfoModal extends Vue {
+  @Prop() title!: string;
+  @PropSync("isModalActive", { type: Boolean }) syncedIsModalActive!: boolean;
+}
+</script>
