d0e8c853c0520b757ac305060d26997455ce3724
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-10-04 18:30:12 +0200

    replace ' w/ "

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import Buefy from "buefy";
-import axios from 'axios';
-import VueAxios from 'vue-axios';
+import axios from "axios";
+import VueAxios from "vue-axios";
 import "buefy/dist/buefy.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
