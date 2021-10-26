6b206296392458394bd22c85e4c3192defae57c7
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-10-26 16:01:08 +0200

    add navigation guard to router #10

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,10 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import Home from "../views/Homepage.vue";
+import UserStore from "@/store/modules/UserStore";
+import { getModule } from "vuex-module-decorators";
 
+const moduleStore = getModule(UserStore);
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
@@ -14,11 +17,13 @@ const routes: Array<RouteConfig> = [
     path: "/admin",
     name: "Admin",
     component: () => import("../views/Admin.vue"),
+    meta: { requiresAuthentication: true },
   },
   {
     path: "/my-subjects",
     name: "Subjects",
     component: () => import("../views/MySubjects.vue"),
+    meta: { requiresAuthentication: true },
   },
   {
     path: "/settings",
@@ -30,12 +35,31 @@ const routes: Array<RouteConfig> = [
     name: "Statistiken",
     component: () => import("../views/Statistics.vue"),
   },
+  {
+    path: "/Shibboleth.sso/Login?target=https%3A%2F%2Fvorwahlen.cloudlab.zhaw.ch",
+    name: "Login",
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuthentication)) {
+    if (moduleStore.isUserAuthenticated) {
+      next();
+    } else {
+      next({
+        name: "Login",
+        query: { redirect: to.fullPath },
+      });
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
