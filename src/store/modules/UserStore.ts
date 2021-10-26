46344b93fd46c105e1e6ac9a16d8f400cd71e901
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-10-26 21:10:03 +0200

    fix is-authenticated path #10

--- src/store/modules/UserStore.ts
+++ src/store/modules/UserStore.ts
@@ -14,9 +14,9 @@ export default class UserStore extends VuexModule {
   @Mutation
   async fetchUserDataAsync(): Promise<void> {
     try {
-      const isAuthenticated = (await Vue.axios.get<boolean>("session/is-authenticated")).data;
+      const isAuthenticated = (await Vue.axios.get<boolean>("../session/is-authenticated")).data;
       if (isAuthenticated) {
-        this.user = (await Vue.axios.get<User>("../session/info")).data;
+        this.user = (await Vue.axios.get<User>("/session/info")).data;
         this.isAuthenticated = true;
         this.isStoreInitialized = true;
       }
