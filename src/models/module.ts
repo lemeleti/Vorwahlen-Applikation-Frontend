41b2c16be0030525361d381a6d7c9a59e8016032
meletlea <meletlea@students.zhaw.ch>
2021-11-03 19:26:04 +0100

    #12 module model vervollständigt

--- src/models/module.ts
+++ src/models/module.ts
@@ -1,5 +1,12 @@
 export default interface Module {
   module_no: string;
+  short_module_no: string;
   module_title: string;
+  module_group: string;
+  is_ip_module: boolean;
+  insitute: string;
+  credits: number;
   language: string;
+  full_time_semester_list: Array<number>;
+  part_time_semester_list: Array<number>;
 }
