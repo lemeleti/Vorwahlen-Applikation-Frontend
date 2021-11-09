6112a1b50a9b83d6fada3fa404215939022b48d7
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-11-09 12:04:42 +0100

    add category field to module interface

--- src/models/module.ts
+++ src/models/module.ts
@@ -1,5 +1,8 @@
+import { ModuleCategories } from "./moduleList";
+
 export default interface Module {
   module_no: string;
   module_title: string;
+  category: ModuleCategories;
   language: string;
 }
