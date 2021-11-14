099f54ea8c1b53f583c8b2e99bf58a1fb7673b9b
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-11-14 19:22:43 +0100

    extend module interface

--- src/models/module.ts
+++ src/models/module.ts
@@ -3,6 +3,7 @@ import { ModuleCategories } from "./moduleList";
 export default interface Module {
   module_no: string;
   module_title: string;
+  full_time_semester_list: Array<number>;
   category: ModuleCategories;
   language: string;
 }
