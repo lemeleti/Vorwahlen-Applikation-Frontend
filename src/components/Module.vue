15f0a6534b1c9f79627d034bca4d1fab09cd7c60
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-11-30 19:35:45 +0100

    add computed properties for module selection #14

--- src/components/Module.vue
+++ src/components/Module.vue
@@ -6,17 +6,60 @@
       style="color: black; min-height: 100px"
     >
       <slot name="title">Modultitel</slot>
+      <b-field id="selectModule">
+        <b-checkbox
+          :value="true"
+          type="is-success"
+          v-model="moduleSelected"
+        ></b-checkbox>
+      </b-field>
+      <b-icon
+        icon="info-circle"
+        type="is-info"
+        id="moduleInfo"
+        class="is-clickable"
+        @click.native="forwardToEventoWeb()"
+      ></b-icon>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import ModuleStore from "@/store/modules/ModuleStore";
 import { Component, Vue, Prop } from "vue-property-decorator";
+import { getModule } from "vuex-module-decorators";
 
 @Component
 export default class Module extends Vue {
   @Prop() moduleNo!: string;
-  @Prop() isSelected!: boolean;
   @Prop() color!: string;
+  moduleStore: ModuleStore = getModule(ModuleStore);
+
+  get moduleSelected(): boolean {
+    return this.moduleStore.isModuleSelected(this.moduleNo);
+  }
+
+  set moduleSelected(selected: boolean) {
+    this.moduleStore.updateElection(this.moduleNo);
+  }
+
+  forwardToEventoWeb(): void {
+    //todo replace with evento url
+    window.open("https://www.google.com", "_blank");
+  }
 }
 </script>
+
+<style>
+#selectModule {
+  top: 0;
+  right: -19px;
+  position: absolute;
+}
+
+#moduleInfo {
+  bottom: 0;
+  right: 0px;
+  position: absolute;
+}
+</style>
