10286a51103eb618002dfd7cd7e3e8b4576f1e6c
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-11-30 15:04:42 +0100

    add tilebox component to display tiles in a boxed format #14

---
+++ src/components/TileBox.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="block">
+    <div class="tile is-ancestor is-vertical">
+      <div class="tile notification is-vertical is-radiusless">
+        <div class="subtitle">
+          <strong><slot name="title"></slot></strong>
+        </div>
+        <div
+          class="
+            tile
+            is-flex
+            is-flex-wrap-wrap
+            is-justify-content-start
+            is-align-content-center
+          "
+        >
+          <slot name="content"></slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class TileBox extends Vue {}
+</script>
