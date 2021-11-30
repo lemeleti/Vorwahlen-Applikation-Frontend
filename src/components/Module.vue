c584baa4d125420186db2abe0e0811da00ec4fb7
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-11-30 15:05:54 +0100

    add simple module component to display electable modules #14

---
+++ src/components/Module.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="tile is-parent has-text-centered is-3">
+    <div
+      class="tile is-child notification is-radiusless"
+      :class="color"
+      style="color: black; min-height: 100px"
+    >
+      <slot name="title">Modultitel</slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component
+export default class Module extends Vue {
+  @Prop() moduleNo!: string;
+  @Prop() isSelected!: boolean;
+  @Prop() color!: string;
+}
+</script>
