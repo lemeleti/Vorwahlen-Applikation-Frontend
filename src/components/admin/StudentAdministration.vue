b50b517775a69a671819ded3bbc55b841aec2da8
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-12-06 13:32:45 +0100

    refactor to extend administration component

--- src/components/admin/StudentAdministration.vue
+++ src/components/admin/StudentAdministration.vue
@@ -1,75 +1,53 @@
 <template>
-  <section>
-    <h1 class="title">Studenten</h1>
-    <div class="block">
-      <b-table
-        :data="students"
-        :columns="columns"
-        :loading="dataIsLoading"
-        :debounce-search="1000"
-        checkable
-        :checked-rows.sync="checkedRows"
-        paginated
-        per-page="15"
-      >
-        <template #bottom-left>
-          <b>Ausgewählt</b>: {{ checkedRows.lenght }}
-        </template>
-      </b-table>
-    </div>
-    <div class="buttons">
-      <b-button
-        @click="addUser"
-        label="Erstellen"
-        icon-left="plus"
-        type="is-success"
-      />
-      <b-button
-        @click="importClassList"
-        icon-left="file-upload"
-        type="is-success"
-        label="Klassenliste"
-      />
-      <b-button
-        @click="editSelectedUsers"
-        label="Editieren"
-        type="is-info"
-        icon-left="edit"
-      />
-      <b-button
-        label="Dispensationsliste"
-        type="is-info"
-        icon-left="file-upload"
-        @click="this.importDispensations"
-      >
-      </b-button>
-      <b-button
-        @click="deleteSelectedStudents"
-        label="Löschen"
-        type="is-danger"
-        icon-left="trash"
-      />
-    </div>
-  </section>
+  <Administration
+    @add="addStudent"
+    @edit="editStudent"
+    @deleteSelected="deleteSelectedStudent"
+    :columns.sync="studentColumns"
+    :rows.sync="studentRows"
+    :checkedRows.sync="checkedStudentRows"
+    :isDataLoading.sync="isStudentDataLoading"
+  >
+    <template #title>Studentenverwaltung</template>
+    <template #buttons-right>
+      <div class="level-item">
+        <b-button
+          @click="importClassList"
+          label="Klassenliste"
+          icon-left="file-upload"
+          type="is-success"
+        />
+      </div>
+
+      <div class="level-item">
+        <b-button
+          @click="importDispensations"
+          label="Dispensationen"
+          icon-left="file-upload"
+          type="is-success"
+        />
+      </div>
+    </template>
+  </Administration>
 </template>
 
 <script lang="ts">
-import { Component, Vue, Mixins } from "vue-property-decorator";
+import { Component, Vue } from "vue-property-decorator";
 import Student from "@/models/student";
-import ModuleListUpload from "@/mixins/ExcelSheetUpload";
 import CreateEditStudent from "@/components/admin/CreateEditStudent.vue";
-import { BModalConfig } from "buefy/types/components";
+import Administration from "@/components/admin/Administration.vue";
 
 @Component({
   components: {
     CreateEditStudent,
+    Administration,
   },
 })
-export default class StudentAdministration extends Mixins(ModuleListUpload) {
-  dataIsLoading = true;
-  students: Array<Student> = [];
-  checkedRows: Array<Student> = [];
-  columns = [
+export default class StudentAdministration extends Administration<Student> {
+  isStudentDataLoading = true;
+  studentRows: Array<Student> = [];
+  checkedStudentRows: Array<Student> = [];
+  studentColumns = [
     {
       field: "email",
       label: "E-Mail",
@@ -104,20 +82,13 @@ export default class StudentAdministration extends Mixins(ModuleListUpload) {
     },
   ];
 
-  modalOption: BModalConfig = {
-    parent: this,
-    component: CreateEditStudent,
-    trapFocus: true,
-    hasModalCard: true,
-    canCancel: ["x", "escape"],
-  };
-
   async created(): Promise<void> {
-    this.students = (await Vue.axios.get("students")).data;
-    this.dataIsLoading = false;
+    this.isStudentDataLoading = false;
+    this.modalOption.component = CreateEditStudent;
+    this.studentRows = (await Vue.axios.get<Array<Student>>("students")).data;
   }
 
-  async deleteSelectedStudents(): Promise<void> {
+  async deleteSelectedStudent(): Promise<void> {
     const userConfirmation = await Vue.swal({
       title: "Wollen Sie die ausgewählten Benutzer löschen?",
       text: "Diese Aktion ist irreversibel",
@@ -127,7 +98,7 @@ export default class StudentAdministration extends Mixins(ModuleListUpload) {
     });
 
     if (userConfirmation.isConfirmed) {
-      for (const student of this.checkedRows) {
+      for (const student of this.checkedStudentRows) {
         try {
           (await Vue.axios.delete(`/students/${student.email}`)).data;
         } catch (e) {
@@ -135,8 +106,8 @@ export default class StudentAdministration extends Mixins(ModuleListUpload) {
           console.log(e);
           return;
         }
-        const studentIndex = this.students.indexOf(student);
-        this.students.splice(studentIndex, 1);
+        const studentIndex = this.studentRows.indexOf(student);
+        this.studentRows.splice(studentIndex, 1);
       }
       Vue.swal({
         title: "Benutzer wurden gelöscht",
@@ -145,7 +116,7 @@ export default class StudentAdministration extends Mixins(ModuleListUpload) {
     }
   }
 
-  async addUser(): Promise<void> {
+  async addStudent(): Promise<void> {
     this.modalOption.props = { student: {}, createStudent: true };
     this.$buefy.modal.open(this.modalOption);
   }
@@ -156,9 +127,9 @@ export default class StudentAdministration extends Mixins(ModuleListUpload) {
     await this.importList();
   }
 
-  async editSelectedUsers(): Promise<void> {
+  async editStudent(): Promise<void> {
     this.modalOption.props = {
-      student: this.checkedRows[0],
+      student: this.checkedStudentRows[0],
       createStudent: false,
     };
     this.$buefy.modal.open(this.modalOption);
