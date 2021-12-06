fe7060b8a370f3e0062cb4f0529182e5658b6638
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-12-06 13:32:19 +0100

    add administration component

---
+++ src/components/admin/Administration.vue
@@ -0,0 +1,92 @@
+<template>
+  <section>
+    <h1 class="title">
+      <slot name="title">Titel</slot>
+    </h1>
+    <div class="block">
+      <b-table
+        :data="syncedRows"
+        :columns="syncedColumns"
+        :loading="syncedIsDataLoading"
+        :debounce-search="1000"
+        checkable
+        :checked-rows.sync="syncedCheckedRows"
+        paginated
+        per-page="15"
+      >
+        <template #bottom-left>
+          <b>Ausgewählt</b>: {{ syncedCheckedRows.lenght }}
+        </template>
+      </b-table>
+    </div>
+    <div class="level">
+      <div class="level-left">
+        <div class="level-item">
+          <b-button
+            @click="add"
+            icon-left="plus"
+            label="Erstellen"
+            type="is-success"
+          />
+        </div>
+
+        <div class="level-item">
+          <b-button
+            @click="edit"
+            icon-left="edit"
+            type="is-info"
+            label="Editieren"
+          />
+        </div>
+
+        <div class="level-item">
+          <b-button
+            @click="deleteSelected"
+            icon-left="trash"
+            type="is-danger"
+            label="Löschen"
+          />
+        </div>
+
+        <slot name="buttons-left"></slot>
+      </div>
+
+      <div class="level-right">
+        <slot name="buttons-right"></slot>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Mixins, PropSync } from "vue-property-decorator";
+import ModuleListUpload from "@/mixins/ExcelSheetUpload";
+import { BModalConfig } from "buefy/types/components";
+
+@Component
+export default class ModuleAdministration<T> extends Mixins(ModuleListUpload) {
+  @PropSync("rows") syncedRows!: Array<T>;
+  @PropSync("columns") syncedColumns!: Array<T>;
+  @PropSync("checkedRows") syncedCheckedRows!: Array<T>;
+  @PropSync("isDataLoading") syncedIsDataLoading!: boolean;
+
+  modalOption: BModalConfig = {
+    parent: this,
+    trapFocus: true,
+    hasModalCard: true,
+    canCancel: ["x", "escape"],
+  };
+
+  add(): void {
+    this.$emit("add");
+  }
+
+  edit(): void {
+    this.$emit("edit");
+  }
+
+  deleteSelected(): void {
+    this.$emit("deleteSelected");
+  }
+}
+</script>
