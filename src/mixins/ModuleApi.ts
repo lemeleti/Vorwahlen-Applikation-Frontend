322a0f11ceaee01f9ca8aa6d2794a312f9acc27a
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-12-12 04:12:54 +0100

    add method to fetch evento data

--- src/mixins/ModuleApi.ts
+++ src/mixins/ModuleApi.ts
@@ -1,3 +1,4 @@
+import EventoData from "@/models/eventoData";
 import Module from "@/models/module";
 import { Component, Vue } from "vue-property-decorator";
 import Api from "./Api";
@@ -11,5 +12,11 @@ export default class ModuleApi extends Api<Module> {
 
   async scrapeModuleData(): Promise<void> {
     await Vue.axios.post(`${this.basePath}/eventodata/scrape`);
+  }
+
+  async getEventoDataById(id: string): Promise<EventoData> {
+    return (
+      await Vue.axios.get<EventoData>(`${this.basePath}/${id}/eventodata`)
+    ).data;
   }
 }
