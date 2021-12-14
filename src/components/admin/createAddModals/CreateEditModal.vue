961ae46dc6b04720905d517b81426183f071cb31
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-12-14 21:14:28 +0100

    remove auto width

--- src/components/admin/createAddModals/CreateEditModal.vue
+++ src/components/admin/createAddModals/CreateEditModal.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="modal-card" style="width: auto">
+  <div class="modal-card">
     <header class="modal-card-head">
       <p class="modal-card-title">
         <slot name="title"></slot>
